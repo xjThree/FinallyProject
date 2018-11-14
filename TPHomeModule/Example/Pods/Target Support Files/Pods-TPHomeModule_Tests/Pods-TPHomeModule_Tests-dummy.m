43f6030a67c4a2d96a218c8e6283731a5112d1a1
xjThree <xj250323489@163.com>
2018-11-14 15:43:57 +0800

    Again

--- TPHomeModule/Example/Pods/Target Support Files/Pods-TPHomeModule_Tests/Pods-TPHomeModule_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TPHomeModule_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_TPHomeModule_Tests
-@end
--- TPHomeModule/Example/Pods/Target Support Files/TPHomeModule/TPHomeModule-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_TPHomeModule : NSObject
-@end
-@implementation PodsDummy_TPHomeModule
-@end
